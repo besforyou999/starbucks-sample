eaf621fe7b1c7edd58e346a40e81d16734330b1b
주상민 <besforyou2@gmail.com>
2023-08-18 17:06:14 +0900

    feat: 헤더와 드롭다운 메뉴 - 전역 배지 (GSAP)

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지 서서히 사라지게 하기
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // 배지 다시 보이게 하기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));  
+// _.throttle(함수, 시간) 두 번째 인자로 전달한 콜백함수는 lodash 라이브러리 함수. throttle로 함수가 일정시간에만 실행되도록 제한을 걸 수 있다.
+
